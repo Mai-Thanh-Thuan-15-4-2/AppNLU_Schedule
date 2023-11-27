4f14fedb06f23fd1b3d69b89f102771d9c2051eb
Mai-Thanh-Thuan-15-4-2 <20130127@st.hcmuaf.edu.vn>
2023-11-27 22:04:53 +0700

    Thêm giao diện menu, thông báo, lịch học

--- Components/MenuPane.js
+++ Components/MenuPane.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons'; // Sử dụng thư viện này để tạo biểu tượng
-
-// Giả sử bạn đã tạo các màn hình này
+import Icon from 'react-native-vector-icons/Ionicons';
 import Schedule from './Schedule';
+import Notification from './Notification';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,10 +12,12 @@ const MenuPane = () => {
       screenOptions={({ route }) => ({
         tabBarActiveTintColor: 'tomato',
         tabBarInactiveTintColor: 'gray',
+        tabBarStyle: { justifyContent: "center", display: 'flex'},
         tabBarStyle: { display: 'flex' },
+        tabBarItemStyle: { width: 'auto' },
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-          let iconSize = focused ? 30 : 20; // Kích thước của biểu tượng sẽ tăng lên 30 khi được chọn
+          let iconSize = focused ? 30 : 20;
 
           if (route.name === 'Tiện ích') {
             iconName = focused ? 'apps' : 'apps-outline';
@@ -28,15 +29,14 @@ const MenuPane = () => {
             iconName = focused ? 'person' : 'person-outline';
           }
 
-          // Bạn có thể trả về bất kỳ thành phần nào ở đây!
           return <Icon name={iconName} size={iconSize} color="#000" />;
         },
       })}
     >
-      <Tab.Screen name="Tiện ích" component={Schedule} />
       <Tab.Screen name="Lịch học" component={Schedule} />
-      <Tab.Screen name="Thông báo" component={Schedule} />
-      <Tab.Screen name="Hồ sơ" component={Schedule} />
+      <Tab.Screen name="Tiện ích" component={Schedule}/>
+      <Tab.Screen name="Thông báo" component={Notification}/>
+      <Tab.Screen name="Hồ sơ" component={Schedule}/>
     </Tab.Navigator>
   );
 };
--- Components/Schedule.js
+++ Components/Schedule.js
@@ -1,62 +1,69 @@
-import React, { useEffect } from 'react';
-import { ScrollView, FlatList, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, FlatList, View, Text, Button, TextInput, Modal } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
-import { Calendar } from 'react-native-calendars'; 
+import { Calendar } from 'react-native-calendars';
 import moment from 'moment/min/moment-with-locales';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { Alert } from 'react-native';
 
-const tasks = [
-  { 
-    id: '1', 
-    title: 'An toàn và bảo mật hệ thống thông tin', 
-    time: 'Tiết 4 -7(09:30)', 
-    location: 'P3', 
-    instructor: 'GV: P.D.Long' 
-  },
-  { 
-    id: '2', 
-    title: 'Data Mining', 
-    time: 'Tiết 7 -10(12:15)', 
-    location: 'P5', 
-    instructor: 'GV: T.Q.Việt' 
-  },
-  { 
-    id: '3', 
-    title: 'Data Mining', 
-    time: 'Tiết 7 -10(12:15)', 
-    location: 'P5', 
-    instructor: 'GV: T.Q.Việt' 
-  },
-  { 
-    id: '4', 
-    title: 'Data Mining', 
-    time: 'Tiết 7 -10(12:15)', 
-    location: 'P5', 
-    instructor: 'GV: T.Q.Việt' 
-  },
-  { 
-    id: '5', 
-    title: 'Data Mining', 
-    time: 'Tiết 7 -10(12:15)', 
-    location: 'P5', 
-    instructor: 'GV: T.Q.Việt' 
-  },
-  { 
-    id: '6', 
-    title: 'An toàn và bảo mật hệ thống thông tin', 
-    time: 'Tiết 4 -7(09:30)', 
-    location: 'P3', 
-    instructor: 'GV: P.D.Long' 
-  },
-];
+const allTasks = {
+  '2023-11-27': [
+    {
+      id: '1',
+      title: 'An toàn và bảo mật hệ thống thông tin',
+      time: 'Tiết 4 -7(09:30)',
+      location: 'P3',
+      instructor: 'GV: P.D.Long',
+      status: 1
+    },
+    {
+      id: '2',
+      title: 'Data Mining',
+      time: 'Tiết 7 -10(12:15)',
+      location: 'P5',
+      instructor: 'GV: T.Q.Việt',
+      status: 1
+    },
+  ],
+  '2023-11-28': [
+    {
+      id: '1',
+      title: 'An toàn và bảo mật hệ thống thông tin',
+      time: 'Tiết 4 -7(09:30)',
+      location: 'P3',
+      instructor: 'GV: P.D.Long',
+      status: 1
+    },
+    {
+      id: '2',
+      title: 'Data Mining',
+      time: 'Tiết 7 -10(12:15)',
+      location: 'P5',
+      instructor: 'GV: T.Q.Việt',
+      status: 1
+    },
+  ],
+};
+
 const data = [
   { label: 'Học kỳ 1 Năm học 2023-2024', value: '1' },
   { label: 'Học kỳ 2 Năm học 2023-2024', value: '2' },
 ];
 
 const Schedule = () => {
-  const [selectedValue, setSelectedValue] = React.useState(null);
-  const [selectedDate, setSelectedDate] = React.useState({});
+  const [selectedValue, setSelectedValue] = useState(null);
+  const [selectedDate, setSelectedDate] = useState({
+    [moment().format('YYYY-MM-DD')]: {
+      selected: true,
+      selectedColor: 'blue',
+    },
+  });
+
+  const [tasks, setTasks] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [isModal2Visible, setModal2Visible] = useState(false);
+  const [newTask, setNewTask] = useState({});
+  const [currentDay, setCurrentDay] = useState('');
 
   const onDayPress = (day) => {
     setSelectedDate({
@@ -65,39 +72,86 @@ const Schedule = () => {
         selectedColor: 'blue',
       },
     });
+    setCurrentDay(day.dateString);
+    setTasks(allTasks[day.dateString] || []);
   };
-  const generateMarkedDates = () => {
-    const startDate = moment('2023-11-01');
-    const endDate = moment('2023-11-30');
-    const markedDates = {};
-  
-    for (let m = moment(startDate); m.isBefore(endDate); m.add(1, 'days')) {
-      if (m.day() === 0) { 
-        markedDates[m.format('YYYY-MM-DD')] = {
-          dots: Array(3).fill({ color: 'red' }),
-        };
-      }
+  const addTask = () => {
+    if (!allTasks[currentDay]) {
+      allTasks[currentDay] = [];
     }
+    const newId = allTasks[currentDay].length + 1;
+    const newTaskWithId = { ...newTask, id: newId.toString(), status: 0 };
+    allTasks[currentDay].push(newTaskWithId);
+    setTasks(allTasks[currentDay]);
+    setNewTask({});
+    setShowModal(false);
+  };
+  const deleteTask = (id) => {
+    Alert.alert(
+      "Xác nhận xóa",
+      "Bạn có chắc chắn muốn xóa công việc này không?",
+      [
+        {
+          text: "Hủy",
+          style: "cancel"
+        },
+        { 
+          text: "Xóa", 
+          onPress: () => {
+            const index = allTasks[currentDay].findIndex(task => task.id === id);
+            if (index !== -1) {
+              allTasks[currentDay].splice(index, 1);
+            }
+            setTasks([...allTasks[currentDay]]);
+          } 
+        }
+      ]
+    );
+  };
+  const editTask = (task) => {
+    const taskIndex = allTasks[currentDay].findIndex(t => t.id === task.id);
   
+    if (taskIndex !== -1) {
+      allTasks[currentDay][taskIndex] = task;
+    }
+    setTasks([...allTasks[currentDay]]);
+  };
+
+
+  const generateMarkedDates = () => {
+    const markedDates = {};
+
+    for (const date in allTasks) {
+      markedDates[date] = {
+        dots: Array(allTasks[date].length).fill({ color: 'green' }),
+      };
+    }
+
     return markedDates;
   };
-  
+
+  useEffect(() => {
+    const today = moment().format('YYYY-MM-DD');
+    setCurrentDay(today);
+    setTasks(allTasks[today] || []);
+  }, []);
+
   const markedDates = generateMarkedDates();
-  
-   return (
-    <View  style={{
+
+  return (
+    <View style={{
       paddingHorizontal: 10,
     }}>
       <Dropdown
-        style={{ 
-          width: '100%', 
-          height: 40, 
-          borderWidth: 1, 
-          borderColor: 'black', 
-          borderRadius: 10, 
+        style={{
+          width: '100%',
+          height: 40,
+          borderWidth: 1,
+          borderColor: 'black',
+          borderRadius: 10,
           paddingHorizontal: 10,
-          justifyContent: 'center', 
-          alignItems: 'center', 
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
         data={data}
         search
@@ -108,59 +162,149 @@ const Schedule = () => {
         value={selectedValue}
         onChange={(item) => setSelectedValue(item.value)}
       />
-      
-     <Calendar
-  style={{ 
-    backgroundColor: 'black',
-    marginTop: 10,
-    borderRadius: 10
-  }}
-  markingType='multi-dot'
-  markedDates={{ ...markedDates, ...selectedDate }}
-  current={moment().format('YYYY-MM-DD')} 
-  onDayPress={onDayPress}
-  hideExtraDays
-  theme={{
-    'stylesheet.calendar.main': {
-      dayToday: {
-        borderWidth: 2,
-      },
-    },
-    'stylesheet.day.basic': {
-      today: {
-        backgroundColor: '#CFE6E6',
-      },
-      sunday: {
-        color: 'red',
-        fontWeight: 'bold',
-      },
-    },
-  }}
-  
-/>
-<FlatList 
-      data={tasks}
-      keyExtractor={item => item.id}
-      style={{height: 310}}
-      renderItem={({ item }) => (
-        
-        <View style={{ 
+
+      <Calendar
+        style={{
+          backgroundColor: 'white',
           marginTop: 10,
-          width: '100%', 
-          height: 66, 
-          borderWidth: 1, 
-          borderColor: 'black', 
-          borderRadius: 10, 
-          paddingHorizontal: 10}}>
-          
-          <Text><Icon name="book-outline" size= {15} color="#000"/>: <Text style={{fontWeight: 'bold'}}>{item.title}</Text></Text>
-          <Text><Icon name="alarm-outline" size= {15} color="#000"/>: <Text style={{color: 'red'}}>{item.time}, {item.location}</Text></Text>
-          <Text><Icon name="person-outline" size= {15} color="#000"/>: <Text>{item.instructor}</Text></Text>
+          borderRadius: 10
+        }}
+        markingType='multi-dot'
+        markedDates={{ ...markedDates, ...selectedDate }}
+        current={moment().format('YYYY-MM-DD')}
+        onDayPress={onDayPress}
+        hideExtraDays
+        theme={{
+          'stylesheet.calendar.main': {
+            dayToday: {
+              borderWidth: 2,
+            },
+          },
+          'stylesheet.day.basic': {
+            sunday: {
+              color: 'red',
+              fontWeight: 'bold',
+            },
+          },
+        }}
+
+      />
+      <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginRight: 10, alignItems: 'center' }}>
+        <View style={{ marginTop: 5, borderWidth: 2, width: 200, height: 30, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderRadius: 5, borderColor: 'gray' }}>
+          <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: 'green', marginRight: 5 }} />
+          <Text>Ngày học</Text>
+          <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: 'red', marginRight: 5, marginLeft: 10 }} />
+          <Text>Ngày thi</Text>
         </View>
-      )}
-    />
+        <TouchableOpacity style={{ width: 20, borderRadius: 5, marginTop: 5, backgroundColor: 'black', alignItems: 'center', marginLeft: 10 }} onPress={() => setShowModal(true)}>
+          <Text style={{ color: 'white' }}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <Modal visible={showModal} transparent={true} animationType="slide">
+        <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.5)', justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
+            <View style={{ marginTop: 10, alignItems: 'center', justifyContent: 'center' }}>
+              <Text style={{ fontWeight: 'bold', color: 'green', alignItems: 'center', justifyContent: 'center' }}>THÊM LỊCH HỌC</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}>
+              <Text style={{ fontWeight: 'bold' }}>Tên môn học:</Text>
+              <TextInput style={{ marginLeft: 10, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }} onChangeText={text => setNewTask(prev => ({ ...prev, title: text }))} maxLength={30}/>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+              <Text style={{ fontWeight: 'bold' }}>Thời gian:</Text>
+              <TextInput style={{ marginLeft: 34, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }} onChangeText={text => setNewTask(prev => ({ ...prev, time: text }))} maxLength={20} />
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+              <Text style={{ fontWeight: 'bold' }}>Phòng học:</Text>
+              <TextInput style={{ marginLeft: 25, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }} onChangeText={text => setNewTask(prev => ({ ...prev, location: text }))} maxLength={15}/>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+              <Text style={{ fontWeight: 'bold' }}>Giảng viên:</Text>
+              <TextInput style={{ marginLeft: 26, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }} onChangeText={text => setNewTask(prev => ({ ...prev, instructor: text }))} maxLength={30}/>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
+              <TouchableOpacity style={{ marginLeft: 100, width: 80, borderRadius: 5, height: 30, marginTop: 5, backgroundColor: '#003', alignItems: 'center', justifyContent: 'center' }} onPress={() => setShowModal(false)}>
+                <Text style={{ color: 'white' }}>Quay lại</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{ marginLeft: 90, width: 80, borderRadius: 5, height: 30, marginTop: 5, backgroundColor: 'blue', alignItems: 'center', justifyContent: 'center' }} onPress={addTask}>
+                <Text style={{ color: 'white' }}>Thêm</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <FlatList 
+  data={tasks}
+  keyExtractor={item => item.id.toString()}
+  style={{height: 270}}
+  renderItem={({ item }) => (
+    <View style={{ 
+      marginTop: 10,
+      width: '100%', 
+      height: 66, 
+      borderWidth: 1, 
+      borderColor: 'black', 
+      borderRadius: 10, 
+      paddingHorizontal: 10}}>
+      
+      <View style={{ flexDirection: 'row' }}>
+        <Text><Icon name="book-outline" size={15} color="#000" />: <Text style={{ fontWeight: 'bold' }}>{item.title}</Text></Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', flex: 1 }}>
+          <TouchableOpacity onPress={() => { setNewTask(item); setModal2Visible(true); }} disabled={item.status === 1}>
+            <Icon name="create-outline" size={20} color={item.status === 1 ? 'gray' : 'blue'} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => deleteTask(item.id)} style={{ marginLeft: 10 }} disabled={item.status === 1}>
+            <Icon name="trash" size={20} color={item.status === 1 ? 'gray' : 'red'} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Text><Icon name="alarm-outline" size={15} color="#000" />: <Text style={{ color: 'red' }}>{item.time}, {item.location}</Text></Text>
+      <Text><Icon name="person-outline" size={15} color="#000" />: <Text>{item.instructor}</Text></Text>
     </View>
-   
+  )}
+/>
+<Modal visible={isModal2Visible} transparent={true} animationType="slide">
+        <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.5)', justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
+            <View style={{ marginTop: 10, alignItems: 'center', justifyContent: 'center' }}>
+              <Text style={{ fontWeight: 'bold', color: 'green', alignItems: 'center', justifyContent: 'center' }}>SỬA LỊCH HỌC</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}>
+              <Text style={{ fontWeight: 'bold' }}>Tên môn học:</Text>
+              <TextInput style={{ marginLeft: 10, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }}   onChangeText={text => setNewTask(prev => ({ ...prev, title: text }))}
+  value={newTask.title} maxLength={30}/>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+              <Text style={{ fontWeight: 'bold' }}>Thời gian:</Text>
+              <TextInput style={{ marginLeft: 34, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }} onChangeText={text => setNewTask(prev => ({ ...prev, time: text }))}
+  value={newTask.time} maxLength={20} />
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+              <Text style={{ fontWeight: 'bold' }}>Phòng học:</Text>
+              <TextInput style={{ marginLeft: 25, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }} onChangeText={text => setNewTask(prev => ({ ...prev, location: text }))}
+  value={newTask.location} maxLength={15}/>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+              <Text style={{ fontWeight: 'bold' }}>Giảng viên:</Text>
+              <TextInput style={{ marginLeft: 26, height: 30, width: '72%', borderColor: 'gray', borderWidth: 1, borderRadius: 5 }} onChangeText={text => setNewTask(prev => ({ ...prev, instructor : text }))}
+  value={newTask.instructor} maxLength={30}/>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
+              <TouchableOpacity style={{ marginLeft: 100, width: 80, borderRadius: 5, height: 30, marginTop: 5, backgroundColor: '#003', alignItems: 'center', justifyContent: 'center' }} onPress={() => setModal2Visible(false)}>
+                <Text style={{ color: 'white' }}>Quay lại</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{ marginLeft: 90, width: 80, borderRadius: 5, height: 30, marginTop: 5, backgroundColor: 'blue', alignItems: 'center', justifyContent: 'center' }} onPress={() => {
+    editTask(newTask);
+    setModal2Visible(false);
+  }}>
+                <Text style={{ color: 'white' }}>Cập nhật</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+
   );
 };
 
